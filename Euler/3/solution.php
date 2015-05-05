407c2131cdc2d0fc32be67ac4a5393260bdc3178
Matt Ford <mat84@btinternet.com>
2015-05-05 15:00:03 +0100

    Solved Problem #3

--- Euler/3/solution.php
+++ Euler/3/solution.php
@@ -1,24 +1,27 @@
 <?php
-function isPrime($number) {
-    for ($i = 1; $i < $number; $i++) {
-        if ($number % $i === 0) {
-            return false;
-        }
-    }
-    return true;
-}
 $number = 600851475143;
-$sqrt = sqrt($number);
-$largest = 0;
-for ($i = 2; $i < $sqrt; $i++) {
-    if ($number % $i === 0) {
-        $curFound = $i;
-        $curFound2 = $number/$i;
-        if (isPrime($curFound) && isPrime($curFound2) && $curFound > $largest) {
-            $largest = $curFound;
-        }
+$start = microtime(true);
+echo "Finding largest prime factor of $number\n";
+
+$i = 2;
+$currentNumber = $number;
+$largestPrime = 0;
+while ($i * $i <= $currentNumber) {
+    if (fmod($currentNumber,$i) == 0) {
+        $largestPrime = $i;
+        $currentNumber = $currentNumber/$i;
+    } else {
+        $i++;
     }
 }
-print $largest;
+if ($currentNumber > $largestPrime) {
+    $largestPrime = $currentNumber;
+}
+echo "The largest prime factor of $number is $largestPrime\n";
+$end = microtime(true);
+$duration = $end - $start;
+echo "Ran in $duration ms";
+
+
 
     
